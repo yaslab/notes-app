e1f76f7499a094459be7fb09df30b8bd7a5d2bb1
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2025-09-28 09:54:54 +0900

    Create a new project using Xcode 26

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Yasuhiro Hatta on 2025/09/28.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,16 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by Yasuhiro Hatta on 2025/09/28.
+//
+
+import Testing
+
+struct NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
